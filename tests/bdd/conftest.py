6273e503baed48f4bde5126194f2050f5de3cbad
NormanMueller <93076064+NormanMueller@users.noreply.github.com>
2023-04-10 15:21:42 +0200

    fix renaming

--- tests/bdd/conftest.py
+++ tests/bdd/conftest.py
@@ -2,7 +2,7 @@ import sys
 
 import unittest.mock
 from pytest_bdd import given, parsers, scenario, then, when
-from game.actual_game import Game, ActionPhase, Player
+from game.actual_game import Game, ActionPhase, Player, Turn
 
 
 @given(parsers.parse("Card is {default_card}"), target_fixture="card")
@@ -16,10 +16,8 @@ def player_with_cards(default_card, card):
     start_deck = [card] * 10
     player = Player("test", start_deck)
     player.deck.draw(5)
-    x = Game(player,player,start_deck)
 
     return player
-
 
 @when(parsers.parse("ActionPhase {default_card} is played"), target_fixture="player")
 def action_phase(default_card, player):
@@ -28,11 +26,11 @@ def action_phase(default_card, player):
 
     with unittest.mock.patch("builtins.input", return_value=default_card):
         action_phase = ActionPhase()
-        action_phase.play_card(player, 1)
+        action_phase.play_card(player, Turn())
         return player
 
 
-@then(parsers.parse("{default_card} card is in cards_inplay pile {bool_value}"))
+@then(parsers.parse("{default_card} card is in cards_in_play pile {bool_value}"))
 def after_action_phase(bool_value, card, player):
     if bool_value:
         assert card in player.deck.cards_in_play.card_list
